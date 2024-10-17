8a2fd98709c0aaf1bdea4072879dec8b4deccc98
Youssef Samy <office.youssefkassem@gmail.com>
2024-10-17 22:17:08 +0300

    quick testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState } from "react";
 
 const App = () => {
+function handleClick() {
+  alert('You clicked me!');
+}
+
 return (
+  <>
   <div class="centered">
    <img
     src="http://localhost:5000/video_feed"
     alt="Video"
    />
   </div>
+  <button onClick={handleClick}>
+  Click me
+  </button>
+  </>
  );
 };
+
+
 
 export default App;
