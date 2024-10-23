6b1a283ed2696253f193c0e826c3c736f66cc3d0
Youssef Samy <office.youssefkassem@gmail.com>
2024-10-23 19:11:21 +0300

    Adding changes from backend

--- backend/robot/vision/camera.py
+++ backend/robot/vision/camera.py
@@ -1,24 +1,54 @@
 import cv2
+from os import name as platform
+from cv2_enumerate_cameras import enumerate_cameras
 
 
 class VideoCamera(object):
-    def __init__(self, camera_number, decoder_func):
-        self.video = cv2.VideoCapture(0)
-        self.video.set(3, 640)
-        self.video.set(4, 480)
-        self.send_for_decode = decoder_func
-        self.scanning = False
+    def __init__(self, scanner_function, camera_number=0):
+        self.__preferred_enum_apis = {
+            "nt": cv2.CAP_MSMF,
+            "posix": cv2.CAP_GSTREAMER
+        }  # enumerating API preferences according to OS
+        self.__enum_api = self.__preferred_enum_apis[platform]
+        self.__cameras = []
+        self.__pause = False  # stream should start out unpaused
+        self.__scanning = False  # scanning should be turned on on-demand
+        self.__send_to_scanner = scanner_function  # called when scanning is on, captured frame is passed as parameter
+        self.__video = cv2.VideoCapture()
+        self.set_source(camera_number)
+        self.__video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.__video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+    def set_source(self, source_index):
+        # TODO: add handling for no cameras found
+        if source_index > len(self.__cameras) or source_index < 0:
+            source_index = 0 # permissive input validation - input is not user dependent
+        self.__video.open(source_index, apiPreference=self.__enum_api)
+
+    def get_sources(self):
+        for cam in enumerate_cameras(self.__enum_api):
+            self.__cameras = []
+            self.__cameras.append(cam.name)
+        return self.__cameras
+
+    def pause_resume_stream(self):
+        self.__pause = not self.__pause
+        return self.__pause
+    
     def toggle_scanning(self):
-        self.scanning = not self.scanning
-        return self.scanning
-
-    def __del__(self):
-        self.video.release()
+        self.__scanning = not self.__scanning
+        return self.__scanning
 
     def get_frame(self):
-        success, frame = self.video.read()
-        if self.scanning:
-            self.send_for_decode(frame)
-        success, jpeg = cv2.imencode(".jpg", frame)
+        if self.__pause:
+            return b""
+        _, frame = self.__video.read()  # TODO: add more error handling
+        if self.__scanning:
+            self.__send_to_scanner(frame)
+        if frame is None:
+            return b""
+        _, jpeg = cv2.imencode(".jpg", frame)
         return jpeg.tobytes()
+
+    def __del__(self):
+        self.__video.release()
--- backend/robot/vision/decoding.py
+++ backend/robot/vision/decoding.py
@@ -1,27 +1,87 @@
 import cv2
 import numpy as np
-from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import decode as pzb_decode_frame
+from pyzbar.pyzbar import ZBarSymbol
+from qreader import QReader
+
+cv_decoder = cv2.QRCodeDetector()
+cv_decode_frame = cv_decoder.detectAndDecodeMulti
+qreader_decoder = QReader()
+qreader_decode_frame = qreader_decoder.detect_and_decode
 
 
 class QR:
-    def __init__(self, receiver_func):
-        self.decoder = "pyzbar"
-        self.send_code = receiver_func
 
-    def decode(self, frame):
-        for qrcode in decode(frame):
-            code = str(qrcode.data, "utf-8")
-            self.send_code(code)
+    def __init__(self, receiver_function, algorithm="pyzbar"):
+        self.__decoders = { "pyzbar": self.__pzb_algorithm, "cv": self.__cv_algorithm, "qreader": self.__qreader_algorithm}
+        self.decode = self.__decoders[algorithm]
+        self.set_receiver(receiver_function)
+
+    def set_receiver(self, receiver_function):
+        self.__send_text = receiver_function
+
+    def set_decoder(self, algorithm="pyzbar"):
+        self.decode = self.__decoders[algorithm]
+
+    # Most efficient, great detection and decoding, poor handling of angled codes
+    def __pzb_algorithm(self, frame):
+        for qrcode in pzb_decode_frame(frame, symbols=[ZBarSymbol.QRCODE]):
+            text = str(qrcode.data, "utf-8")
+            obj_id = self.__send_text(text)
             pts = np.array([qrcode.polygon], np.int32)
-            pts = pts.reshape(-1, 1, 2)
-            cv2.polylines(frame, [pts], True, (255, 0, 0), 3)
-            pts2 = qrcode.rect
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 3)
+            corners = qrcode.rect  # top-left corner's coordinates
             cv2.putText(
                 frame,
-                code,
-                (pts2[0], pts2[1]),
+                obj_id,
+                (corners[0], corners[1]),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                0.9,
-                (255, 0, 255),
-                2,
+                0.8,
+                (0, 255, 0),
+                1,
+                cv2.LINE_AA,
             )
+
+    # Least efficient, worse decoding in general, better handling of angled codes
+    def __cv_algorithm(self, frame):
+        detected, decoded_text, points, _ = cv_decode_frame(frame)
+        if detected:
+            for text, pts in zip(decoded_text, points):
+                if text:
+                    obj_id = self.__send_text(text)
+                    color = (0, 255, 0)
+                    max_y = None  # y decreases as you go up somehow
+                    for p in pts.astype(int):
+                        p = p.tolist()
+                        if max_y is None:
+                            max_y = p[1]
+                            corner = tuple(p)
+                        elif max_y > p[1]:
+                            corner = tuple(p)
+                            max_y = p[1]
+                    cv2.putText(
+                        frame,
+                        obj_id,
+                        corner,
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.8,
+                        color,
+                        1,
+                        cv2.LINE_AA,
+                    )
+                else:
+                    color = (0, 0, 255)
+                cv2.polylines(frame, [pts.astype(int)], True, color, 3)
+
+    # Most resource intensive, most accurate
+    def __qreader_algorithm(self, frame):
+        decoded_text, code_geometry = qreader_decode_frame(image=frame, return_detections=True)
+        for i, qr in enumerate(decoded_text):
+            if qr is not None:
+                color = (0, 255, 0)
+                obj_id = self.__send_text(qr)
+                corner = code_geometry[i]["bbox_xyxy"].astype(int)
+                cv2.putText(frame, obj_id, (corner[0], corner[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1, cv2.LINE_AA)
+            else:
+                color = (0, 0, 255)
+            cv2.polylines(frame, [code_geometry[i]["polygon_xy"].astype(int)], True, color, 3)
