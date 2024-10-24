8e83eff9ec6266cb0d12aade0baa9a6aea3523b0
ahmed <a.samehpsn@gmail.com>
2024-10-24 19:54:38 +0200

    gamepad sent

--- backend/api/events/gamepad.py
+++ backend/api/events/gamepad.py
@@ -1,6 +1,10 @@
 from . import socketio
-import paho.mqtt.publish as publish
+import json
+
+#import paho.mqtt.publish as publish
 @socketio.on("gamepad buttons")
-def map_and_send_buttons(data):
+def map_and_send_buttons(command, commandValue):
+  print(command)
+  print(commandValue)
     # ... manipulate data
-    publish.single("navigation/send_controls", data) # to localhost:1883
+#    publish.single("navigation/send_controls", data) # to localhost:1883
