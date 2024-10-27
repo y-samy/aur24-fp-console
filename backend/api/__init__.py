978bac8510f530703b9c3733166ec2ed32d1390c
AHmad-Hossam-88 <ahmad.hossam.8818@gmail.com>
2024-10-27 21:46:20 +0300

    h

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -7,6 +7,8 @@ from robot.vision.camera import VideoCamera
 from robot.vision.decoding import QR
 
 from .events.mqtt import mqttc
+from .events import *
+from .routes import video_routes
 
 box_handler = BoxHandler()
 qr_decoder = QR(receiver_function=box_handler.receive_coords)
@@ -17,7 +19,6 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
 
 # api routes
-from .routes import video_routes
 app.register_blueprint(video_routes, url_prefix='/video')
 
 # mqtt
@@ -25,6 +26,5 @@ mqttc.connect(host="localhost")
 mqttc.loop_start()
 
 # api events
-from .events import *
 box_handler.set_callback_on_new_box(push_new_box)
 
--- backend/api/events/localization.py
+++ backend/api/events/localization.py
@@ -2,11 +2,12 @@ import requests
 from json import loads as jsonify
 
 
-def handle_encoder_data():
-    imu_data = jsonify((requests.get("http://192.168.172.75:8080/get?linX&linY&gyrZ")).content)["buffer"]
-    
-    readings = ["linX", "linY", "gyrZ"]
-    for i in readings:
-        print(f"{i},{imu_data[i]["buffer"][0]}")
-        
+def handle_encoder_data(encoderData):
+    # imu_data = jsonify(
+    #     (requests.get("http://192.168.172.75:8080/get?linX&gyrZ")).content
+    # )["buffer"]
 
+    # readings = ["linX", "gyrZ"]
+    # aX = imu_data["linX"]["buffer"][0]
+    # gZ = imu_data["gyrZ"]["buffer"][0]
+    print(encoderData)
